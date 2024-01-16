26c4c253886f95cbc0479b6d06aa8f939ef4e335
Michael <mquinter88@gmail.com>
2024-01-16 18:07:05 -0500

    init

---
+++ src/test/java/org/mcq/spring6reactive/Spring6ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mcq.spring6reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
